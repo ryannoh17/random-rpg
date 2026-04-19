5460138861d25c511a2ffbd5e06355712eeabafb
Ryan Noh <ryannoh17@gmail.com>
2026-04-19 17:04:30 -0400

    added monster spawn mechanics to monster class

--- src/classes/monster.ts
+++ src/classes/monster.ts
@@ -1,5 +1,4 @@
 import type { ItemType } from "../schemas/item.js";
-import type { MonsterType } from "../schemas/monster.js";
 
 export const MonsterList = [];
 
@@ -20,7 +19,6 @@ export class Monster {
   constructor(
     name: string,
     maxHealth: number,
-    health: number,
     attack: number,
     drops: ItemType[],
     level: number,
@@ -28,7 +26,7 @@ export class Monster {
   ) {
     this.name = name;
     this.maxHealth = maxHealth;
-    this.health = health;
+    this.health = maxHealth;
     this.attack = attack;
     this.drops = drops;
     this.level = level;
@@ -55,19 +53,61 @@ export class Monster {
 
     const Dummy: spawnRate[] = [
       {
-        monster: new Monster("Dummy", 30, 30, 0, [], 0, "Dummy"),
+        monster: new Monster("Dummy", 30, 0, [], 0, "Dummy"),
         weight: 1
       }
     ]
 
-    // const sunlitMeadow = [
-    //   { monsterName: "Slime", weight: 60 },
-    //   { monsterName: "Horned Rabbit", weight: 39 },
-    //   { monsterName: "Wild Tusk", weight: 1 }
-    // ];
+    const SunlitMeadow: spawnRate[] = [
+      {
+        monster: new Monster('Slime', 60, 5, [], 1, "Sunlit Meadow"),
+        weight: 60
+      },
+      {
+        monster: new Monster('Horned Rabbit', 50, 15, [], 3, "Sunlit Meadow"),
+        weight: 35
+      },
+      {
+        monster: new Monster('Wild Tusk', 110, 20, [], 5, "Sunlit Meadow"),
+        weight: 5
+      }
+    ]
 
-    // switch this shit ig or something
-    return weightedRandom(Dummy);
+    const Greenwood: spawnRate[] = [
+      {
+        monster: new Monster('Goblin', 100, 20, [], 6, "Greenwood"),
+        weight: 60
+      },
+      {
+        monster: new Monster('Dire Wolf', 150, 30, [], 8, "Greenwood"),
+        weight: 35
+      },
+      {
+        monster: new Monster('Orge', 200, 40, [], 10, "Greenwood"),
+        weight: 5
+      }
+    ]
+
+    let monsters: spawnRate[];
+
+    switch (zone) {
+      case "Dummy":
+        monsters = Dummy;
+        break;
+      
+      case "Sunlit Meadow":
+        monsters = SunlitMeadow;
+        break;
+
+      case "Greenwood":
+        monsters = Greenwood;
+        break;
+      
+      default:
+        throw new Error(`the switch statement failed???`);
+    }
+
+    return weightedRandom(monsters);
   }
 }
 
