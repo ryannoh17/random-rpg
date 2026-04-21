1916f58eefe2522635feb2652fa7c8fffb874a40
Ryan Noh <ryannoh17@gmail.com>
2026-04-21 15:01:28 -0400

    healing buttons done

---
+++ src/components/buttons/greaterHealPotion.ts
@@ -0,0 +1,31 @@
+import { ButtonInteraction, EmbedBuilder } from "discord.js";
+import { Profile } from "../../schemas/profile.js";
+import { Player } from "../../classes/Player.js";
+// const { Monster } = require(`../../commands/game/fight`);
+
+export default {
+  data: {
+    name: 'greaterHealPotion',
+  },
+
+  async execute(interaction: ButtonInteraction) {
+    const { user, guild, message } = interaction;
+    const oldEmbed = message.embeds[0]!;
+
+    let player = await Player.load(user.id, guild!.id);
+
+    player.health = player.maxHealth;
+
+    await player.save();
+
+    const newEmbed = EmbedBuilder.from(oldEmbed).spliceFields(3, 1, {
+      name: user.username,
+      value: `${player.health}/${player.maxHealth}`,
+      inline: true,
+    });
+
+    return await interaction.update({
+      embeds: [newEmbed],
+    });
+  },
+};
---
+++ src/components/buttons/healPotion.ts
@@ -0,0 +1,30 @@
+import { ButtonInteraction, EmbedBuilder } from "discord.js";
+import { Player } from "../../classes/Player.js";
+
+export default {
+  data: {
+    name: 'healPotion',
+  },
+
+  async execute(interaction: ButtonInteraction) {
+    const { user, guild, message } = interaction;
+    const oldEmbed = message.embeds[0]!;
+
+    let player = await Player.load(user.id, guild!.id);
+    let monster = player.monster!;
+
+    player.health += 5 - monster.attack;
+
+    await player.save();
+    
+    const newEmbed = EmbedBuilder.from(oldEmbed).spliceFields(3, 1, {
+      name: user.username,
+      value: `${player.health}/${player.maxHealth}`,
+      inline: true,
+    });
+
+    return await interaction.update({
+      embeds: [newEmbed],
+    });
+  },
+};
