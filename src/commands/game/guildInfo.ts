b6a0a371ee79732ba44abc969aab09ffcffbb667
Ryan Noh <ryannoh17@gmail.com>
2026-04-22 00:15:13 -0400

    improved guild info command, why do i have this

--- src/commands/game/guildInfo.ts
+++ src/commands/game/guildInfo.ts
@@ -10,28 +10,28 @@ export default {
   async execute(interaction: ChatInputCommandInteraction) {
     const { guild }  = interaction;
 
-    if (guild == null) {
+    if (guild == null) 
       return interaction.reply('We are not in a server');
-    }
+    
 
     let guildProfile = await GuildModel.findOne({ guildId: guild.id });
 
     if (!guildProfile) {
       // yeah i have no idea why I saved guild data keeping this ig
 
-      guildProfile = await new GuildModel({
+      guildProfile = new GuildModel({
         guildId: guild.id,
         guildName: guild.name,
         guildIcon: guild.iconURL() ? guild.iconURL() : 'none',
       });
 
       await guildProfile.save().catch(console.error);
-      await interaction.reply({
+      return interaction.reply({
         content: `New Server\nServer Name: ${guildProfile.guildName}\nServer ID: ${guildProfile.guildId}`,
       });
       
     } else {
-      await interaction.reply({
+      return interaction.reply({
         content: `Server Name: ${guild.name}\nServer ID: ${guild.id}`,
       });
     }
